943985bfc4f420a488c8d158ec6dd6571985acb4
Theodore Redlon <redlontheo@gmail.com>
2022-11-30 21:38:59 -0500

    Little by little

--- Hangman.py
+++ Hangman.py
@@ -1,55 +1,17 @@
+import Create_Game
 import os
+import random
 
-os.system("cls")
 
-
-class Game:
-    def __init__(self, word):
-        self.word = word
-        self.right_guess = 0
-        self.wrong_guess = 0
-        self.guessed_correct = []
-        self.total_guesses = len(word)
-
-        for letter in word:
-            self.guessed_correct.append("_")
-
-    def display(self):
-        print("Hangman Time")
-        print("Hidden word has {} letters to guess".format(str(len(self.word))))
-        print("{} guesses left!".format(self.total_guesses))
-        temp = ""
-        for spot in self.guessed_correct:
-            temp += spot + " "
-        print(temp)
-
-    def guess_spot(self, guess):
-        self.total_guesses -= 1
-        for idx, letter in enumerate(self.word):
-            if guess == letter:
-                self.guessed_correct[idx] = guess
-
-    def winner(self):
-        for spot in self.guessed_correct:
-            if spot == "_":
-                return False
-        return True
-
-    def loser(self):
-        if self.total_guesses == 0:
-            return True
-        else:
-            return False
-
-    def get_right_guesses(self):
-        for spot in self.guessed_correct:
-            if spot != "_":
-                self.right_guess += 1
-        return self.right_guess
+def get_random_word():
+    with open("words.txt", "r", encoding="utf-8") as f:
+        data = f.read()
+        data = list(data.split("\n"))
+    return data[random.randint(0, 29)]
 
 
 def PLAY():
-    game = Game("yellow")
+    game = Create_Game.Game(get_random_word())
     while True:
         os.system("cls")
         game.display()
